b37e4cd2706f4e19824971eab8a20b18c285c4c9
Daniel Barrera Serna <0206941@up.edu.mx>
2020-08-26 16:35:18 -0500

    Add project files.

---
+++ HolaXamarin/HolaXamarin/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HolaXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var nombre = "Nombre: "+Nombre.Text + " " + "Apellido: " + Apellido.Text;
+            var direccion = "Edad: " + double.Parse(Edad.Text) + " " + "Dirección: " + Dirección.Text;
+
+            if (!string.IsNullOrEmpty(Nombre.Text) &&
+                !string.IsNullOrEmpty(Apellido.Text) &&
+                !string.IsNullOrEmpty(Dirección.Text))
+            {
+
+                DisplayAlert(nombre, direccion, "Ok");
+            }
+            else
+            {
+                DisplayAlert("Datos incompletos",
+                    "Por favor, llena toda la información",
+                    "Ok");
+            }
+
+        }
+    }
+}
